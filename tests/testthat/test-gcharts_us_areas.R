2096b0691819c806649e4756fe52791fb8293e6a
Oscar de León <odeleongt@gmail.com>
2025-05-25 13:30:49 -0400

    Set up testing

---
+++ tests/testthat/test-gcharts_us_areas.R
@@ -0,0 +1,148 @@
+#------------------------------------------------------------------------------*
+# test chart data functions ----
+#------------------------------------------------------------------------------*
+
+
+# set up the tests ----
+
+tmpdir <- gsub("[\\]+", "/", tempdir())
+# save original value
+oldpath <- Sys.getenv("R_GOOGLE_CHART_CACHE")
+Sys.unsetenv("R_GOOGLE_CHART_CACHE")
+
+test_that("error if no cache set", {
+  expect_error(
+    gchart_get_us_areas(500L),
+    regexp = "cache"
+  )
+})
+
+
+# use a temporary cache
+Sys.setenv(R_GOOGLE_CHART_CACHE = tmpdir)
+
+
+#------------------------------------------------------------------------------*
+# test GET interface ----
+#------------------------------------------------------------------------------*
+
+test_that("expects integers", {
+  expect_error(
+    gchart_get_us_areas(500),
+    regexp = "integer"
+  )
+})
+
+
+test_that("expects at least one area for the request", {
+  expect_error(
+    gchart_get_us_areas(integer(0)),
+    regexp = "at least"
+  )
+})
+
+
+test_that("respect limit", {
+  expect_error(
+    gchart_get_us_areas(500L, limit = 0),
+    regexp = "fetch more than"
+  )
+})
+
+
+test_that("succeeds with known area", {
+  expect_equal(
+    gchart_get_us_areas(500L),
+    file.path(tmpdir, "us-areas", "500.js"),
+    ignore_attr = TRUE
+  )
+})
+
+
+test_that("succeeds with existing file", {
+  expect_equal(
+    gchart_get_us_areas(500L),
+    file.path(tmpdir, "us-areas", "500.js"),
+    ignore_attr = TRUE
+  )
+  unlink(file.path(tmpdir, "us-areas", "500.js"))
+})
+
+
+test_that("fails with incorrect area", {
+  gchart_get_us_areas(-1L)
+  path <- gchart_available_areas()
+  content <- readLines(path, n = 1, warn = FALSE)
+  expect_equal(
+    content,
+    "error: 404",
+    ignore_attr = TRUE
+  )
+  unlink(file.path(tmpdir, "us-areas", "-1.js"))
+  rm(path, content)
+})
+
+
+
+
+#------------------------------------------------------------------------------*
+# test geo data processing ----
+#------------------------------------------------------------------------------*
+
+gchart_get_us_areas(500L)
+
+
+test_that("listing available areas needs cache", {
+  expect_error(
+    gchart_available_areas(cache = ""),
+    regexp = "Cache should be used"
+  )
+})
+
+
+test_that("processing needs cache", {
+  expect_error(
+    gchart_process_us_areas(1L, cache = ""),
+    regexp = "Cache should be used"
+  )
+})
+
+
+test_that("processing expects integers", {
+  expect_error(
+    gchart_process_us_areas(500),
+    regexp = "integer"
+  )
+})
+
+
+test_that("processing expects at least one area for the request", {
+  expect_error(
+    gchart_process_us_areas(integer(0)),
+    regexp = "at least"
+  )
+})
+
+
+test_that("missing areas requested for processing", {
+  expect_error(
+    gchart_process_us_areas(-1L),
+    regexp = "requested areas should be downloaded"
+  )
+})
+
+
+test_that("processes available areas", {
+  expect_equal(
+    class(gchart_process_us_areas(areas = 500L))[1],
+    "sf"
+  )
+})
+
+
+
+
+
+
+unlink(file.path(tmpdir, "us-areas", "500.js"))
+Sys.setenv(R_GOOGLE_CHART_CACHE = oldpath)
---
+++ tests/testthat/test-helpers.R
@@ -0,0 +1,160 @@
+
+tmpdir <- gsub("[\\]+", "/", tempdir())
+unlink(file.path(tmpdir, ".Renviron"))
+# save original value
+oldpath <- Sys.getenv("R_GOOGLE_CHART_CACHE")
+Sys.unsetenv("R_GOOGLE_CHART_CACHE")
+
+test_that("finds the path existing root", {
+  expect_equal(
+    path_existing_root(tmpdir),
+    tmpdir
+  )
+})
+
+
+test_that("ignores non-existing dirs", {
+  expect_equal(
+    path_existing_root(file.path(tmpdir, "non/existent/path")),
+    tmpdir
+  )
+})
+
+
+test_that("finds home", {
+  expect_equal(
+    path_existing_root(path = "HOME"),
+    gsub("[\\]+", "/", Sys.getenv("HOME"))
+  )
+})
+
+
+test_that("finds writeable home", {
+  expect_equal(
+    gchart_cache_dir(path = "HOME"),
+    gsub("[\\]+", "/", Sys.getenv("HOME"))
+  )
+})
+
+
+test_that("creates a new path", {
+  if(
+    expect_true(
+      file.exists(gchart_cache_dir(file.path(tmpdir, "gchart/test/path")))
+    )
+  ){
+    # remove the test path
+    unlink(file.path(tmpdir, "gchart"), recursive = TRUE)
+  }
+})
+
+
+test_that("sets the path in environment variables", {
+  expect_equal(
+    suppressMessages(gchart_set_cache(path = tmpdir)),
+    Sys.getenv("R_GOOGLE_CHART_CACHE")
+  )
+  Sys.unsetenv("R_GOOGLE_CHART_CACHE")
+})
+
+
+test_that("installs the path", {
+  expect_true(
+    {
+      suppressMessages(
+        gchart_set_cache(path = tmpdir, install = TRUE, home = tmpdir)
+      )
+      grepl("R_GOOGLE_CHART_CACHE", readLines(file.path(tmpdir, ".Renviron")))
+    }
+  )
+  unlink(file.path(tmpdir, ".Renviron"))
+})
+
+
+test_that("error if path already installed", {
+  expect_error(
+    {
+      suppressMessages(
+        gchart_set_cache(path = tmpdir, install = TRUE, home = tmpdir)
+      )
+      suppressMessages(
+        gchart_set_cache(path = tmpdir, install = TRUE, home = tmpdir)
+      )
+    }
+  )
+  unlink(file.path(tmpdir, ".Renviron"))
+})
+
+
+test_that("removes the path", {
+
+  suppressMessages(gchart_set_cache(path = tmpdir))
+
+  gchart_remove_cache_path(remove = TRUE)
+
+  expect_equal(
+    Sys.getenv("R_GOOGLE_CHART_CACHE"),
+    ""
+  )
+})
+
+
+test_that("removes installed path", {
+  expect_true(
+    {
+      suppressMessages(
+        gchart_set_cache(path = tmpdir, install = TRUE, home = tmpdir)
+      )
+
+      gchart_remove_cache_path(remove = TRUE, path = tmpdir)
+
+      found <- grepl(
+        "R_GOOGLE_CHART_CACHE", readLines(file.path(tmpdir, ".Renviron"))
+      )
+
+      !found || length(found) == 0
+    }
+  )
+  unlink(file.path(tmpdir, ".Renviron"))
+})
+
+
+test_that("overwrites the installed path", {
+  expect_true(
+    {
+      suppressMessages(
+        gchart_set_cache(path = tmpdir, install = TRUE, home = tmpdir)
+      )
+      suppressMessages(
+        gchart_set_cache(
+          path = tmpdir, install = TRUE, overwrite = TRUE, home = tmpdir
+        )
+      )
+
+      grepl("R_GOOGLE_CHART_CACHE", readLines(file.path(tmpdir, ".Renviron")))
+    }
+  )
+  unlink(file.path(tmpdir, ".Renviron"))
+})
+
+
+test_that("gets the saved path", {
+  expect_equal(
+    gchart_get_cache_path(),
+    Sys.getenv("R_GOOGLE_CHART_CACHE")
+  )
+})
+
+
+test_that("returns the valid provided path", {
+  expect_equal(
+    gchart_get_cache_path(path = tmpdir),
+    tmpdir
+  )
+})
+
+
+
+
+# restore original value
+Sys.setenv(R_GOOGLE_CHART_CACHE = oldpath)
